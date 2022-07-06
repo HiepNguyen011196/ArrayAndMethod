651d6e7d78d4a571f8c849ce681581261b07a4d4
Hiep Nguyen <gagga5376@gmail.com>
2022-07-06 22:43:38 +0700

    ThucHanh3

--- src/FindMaxElement.java
+++ src/FindMaxElement.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ReverseArray {
+public class FindMaxElement {
     public static void main(String[] args) {
         int size;
         int[] array;
@@ -18,19 +18,19 @@ public class ReverseArray {
             array[i] = scanner.nextInt();
             i++;
         }
-        System.out.printf("%-20s%s", "Elements in array: ", "");
-        for (int j = 0; j < array.length; j++){
-            System.out.print(array[j] + " ");
-        }
-
-        for (int j = 0; j < array.length / 2; j++) {
-            int temp = array[j];
-            array[j] = array[size - 1 - j];
-            array[size - 1 - j] = temp;
-        }
-        System.out.printf("\n%-20s%s", "Reverse array: ", "");
+        System.out.print("Property list: ");
         for (int k : array) {
             System.out.print(k + " ");
         }
+        System.out.println("");
+        int max = array[0];
+        int index = 0;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] > max) {
+                max = array[j];
+                index = j + 1;
+            }
+        }
+        System.out.println("The largest property value in the list is " + max + " at position " + index);
     }
 }
